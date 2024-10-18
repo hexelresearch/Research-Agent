cb28a061929f086a0913445086be16dbe3784c9a
khushwant04 <khushwantsanwalot2004@outlook.com>
2024-10-18 14:36:28 +0000

    main

---
+++ Infrence/main.py
@@ -0,0 +1,54 @@
+import torch
+from transformers import pipeline
+from fastapi import FastAPI, status
+from fastapi.responses import JSONResponse
+
+def model_setup(model_id: str, torch_dtype, device_map: str):
+    pipe = pipeline(
+        "text-generation",
+        model=model_id,
+        torch_dtype=torch_dtype,
+        device_map=device_map,
+    )
+    return pipe
+
+def messages(prompt):
+    messages = [
+            {"role": "system", "content": "You are a Research Assistant Who always talk with Facts and In depth Knowledge"},
+            {"role": "user", "content": prompt},
+        ]
+    return messages
+
+meta_llama_3b = model_setup("meta-llama/Llama-3.2-3B-Instruct", torch.bfloat16, "cuda")
+meta_llama_1b = model_setup("meta-llama/Llama-3.2-1B-Instruct", torch.bfloat16, "cuda")
+
+app = FastAPI()
+
+@app.get('/')
+def index():
+    return JSONResponse({"message":"Welcome to Hexel AI"}, status_code=status.HTTP_200_OK)
+
+@app.get("/search1b")
+def infrence(prompt: str):
+    try:
+        outputs = meta_llama_3b(
+        messages(prompt),
+        max_new_tokens=1024,
+        )
+        return {"message":outputs[0]["generated_text"][-1]}
+    except Exception as e:
+        return JSONResponse(e)
+         
+@app.get("/search3b")
+def infrence(prompt: str):
+    try:
+        outputs = meta_llama_3b(
+        messages(prompt),
+        max_new_tokens=1024,
+        )
+        return {"message":outputs[0]["generated_text"][-1]}
+    except Exception as e:
+        return JSONResponse(e)
+        
+    
+    
